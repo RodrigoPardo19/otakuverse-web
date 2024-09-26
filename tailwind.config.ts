24b5ab05847a4ace0b620fc6d032565d70f316f3
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2024-09-26 14:52:15 -0300

    add soon page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+				primary: "#ed1c1a",
+				secondary: "#2893a3",
       },
     },
   },
