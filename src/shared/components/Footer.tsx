0a573ac92d8ba915495d292dd166fbe8bb7318ee
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2024-09-30 14:08:36 -0300

    add footer with social media icons

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,13 +1,18 @@
+import { DiscordIcon } from './Icons/DiscordIcon';
+import { GithubIcon } from './Icons/GithubIcon';
+import { InstagramIcon } from './Icons/InstagramIcon';
+import { XIcon } from './Icons/XIcon';
+
 export default function Footer() {
 	return (
-		<footer className="flex flex-col items-center justify-center border-t border-border py-8">
+		<footer className="flex flex-col items-center justify-center gap-4 border-t border-border py-4">
 			<div className="flex gap-8">
-				<p>X</p>
-				<p>Instagram</p>
-				<p>Discord</p>
-				<p>Github</p>
+				<XIcon width="2rem" height="2rem" className="cursor-pointer hover:text-primary" />
+				<InstagramIcon width="2rem" height="2rem" className="cursor-pointer hover:text-primary" />
+				<DiscordIcon width="2rem" height="2rem" className="cursor-pointer hover:text-primary" />
+				<GithubIcon width="2rem" height="2rem" className="cursor-pointer hover:text-primary" />
 			</div>
-			<div>©2024 All Rights Reserved to Otakuverse.</div>
+			<div className="text-sm">©2024 All Rights Reserved to Otakuverse.</div>
 		</footer>
 	);
 }
